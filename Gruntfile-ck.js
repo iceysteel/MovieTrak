4f2ee4a277f19643a67ee32cca2b1bd34f5f6468
Sajad <me@sajad.co>
2014-10-25 16:57:34 -0700

    Installed Node, Bootstrap

---
+++ Gruntfile-ck.js
@@ -0,0 +1,1 @@
+module.exports=function(e){e.initConfig({pkg:e.file.readJSON("package.json"),uglify:{options:{banner:'/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'},build:{src:"src/<%= pkg.name %>.js",dest:"build/<%= pkg.name %>.min.js"}}});e.loadNpmTasks("grunt-contrib-uglify");e.registerTask("default",["uglify"])};
